aafffebe337c30966740b6a1d4d7c5479faa9cc6
Barbara Bermes <bbinto@bbinto-air.local>
2015-01-17 09:59:31 -0500

    more stuff

--- chapter-4/minified/concat.js
+++ chapter-4/minified/concat.js
@@ -23,3 +23,25 @@ else {
      document.write("By the way, this is your first time here.");
 }
 setCookie("counter", visits);
+
+function myFunction() {
+    var x = document.getElementById("demo");
+    x.style.fontSize = "25px";           
+    x.style.color = "red"; 
+}
+var myPix = new Array("images/pathfinder.gif", "images/surveyor.gif", "images/surveyor98.gif");
+var thisPic = 0;
+
+function processPrevious() {
+     if (document.images && thisPic > 0) {
+          thisPic--;
+          document.myPicture.src = myPix[thisPic];
+     }
+}
+
+function processNext() {
+     if (document.images && thisPic < 2) {
+          thisPic++;
+          document.myPicture.src = myPix[thisPic];
+     }
+}
